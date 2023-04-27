3c24e5c41c6a0161967e0ecd6f2d23ae1776b35a
Evandru <evandro_remolli.t@outlook.com>
2023-04-27 15:10:22 -0300

    AutoCompleteSearch API

--- API/API.cs
+++ API/API.cs
@@ -1,11 +1,29 @@
-﻿namespace FoodStore.API
+﻿using FoodStore.Models;
+using Newtonsoft.Json;
+
+namespace FoodStore.API
 {
     public class API
     {
         HttpClient client;
-        public API()
+        string apiKey;
+        public API(string apiKey)
         {
+            this.apiKey = apiKey;
             client = new HttpClient();
+        }
+
+        public async Task<AutoCompleteSearch[]> AutoCompleteSearch (string search)
+        {
+            var response = await client.GetAsync(
+                "https://api.spoonacular.com/recipes/autocomplete?apiKey=" +
+                apiKey + "&number=10&query=" + search);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var autoCompleteContent = JsonConvert.DeserializeObject<AutoCompleteSearch[]>(content);
+
+            return autoCompleteContent;
         }
     }
 }
