6dc3207a2114f5d3623fc88362f7f7038a480367
Evandru <evandro_remolli.t@outlook.com>
2023-04-27 14:48:49 -0300

    AutoCompleteSearch Model

---
+++ Models/AutoCompleteSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodStore.Models
+{
+    public class AutoCompleteSearch
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string imageType { get; set; }
+    }
+}
